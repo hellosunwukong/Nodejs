82f235cfaa99567d674cf6fb56008c5ae9836699
Mc.Test <Test.Mc@gmail.com>
2013-10-23 21:36:50 +0800

    :o
    :q

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
   , http = require('http')
   , path = require('path');
 
@@ -12,11 +11,14 @@ var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
+  app.set('views', __dirname + '/server/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
+  app.locals.pretty = true;
+  // app.use(express.favicon());
+  // app.use(express.logger('dev'));
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'super-duper-secret-secret' }));
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
@@ -25,10 +27,11 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler());
-    app.locals.pretty = true;
+    // app.locals.pretty = true;
 });
 
-app.get('/', routes.index);
+// app.get('/', routes.index);
+require('./server/routes/router')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
